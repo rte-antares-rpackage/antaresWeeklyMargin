4490c4ec207615e2ddfbf01b1a11b03364c69641
pvictor <perrier.victor@gmail.com>
2018-01-23 14:09:51 +0100

    use crul method for proxy

--- R/rte-api.R
+++ R/rte-api.R
@@ -11,7 +11,7 @@
 #' @return a \code{data.table}
 #' @export
 #' 
-#' @importFrom crul HttpClient
+#' @importFrom crul HttpClient proxy
 #' @importFrom curl ie_get_proxy_for_url
 #' @importFrom data.table as.data.table setcolorder :=
 #' @importFrom jsonlite fromJSON
@@ -28,19 +28,14 @@
 #' 
 #' }
 get_eco2mix <- function(from = NULL, to = NULL, user = NULL, proxy_pwd = NULL) {
-  if (!is.null(user) & !is.null(proxy_pwd)) {
-    proxy <- paste0(
-      "http://", user, ":", proxy_pwd, "@",
-      curl::ie_get_proxy_for_url("https://httpbin.org/get")
-    )
-    Sys.setenv(
-      http_proxy = proxy, https_proxy = proxy,
-      HTTP_PROXY = proxy, HTTPS_PROXY = proxy,
-      ftp_proxy = proxy, FTP_PROXY = proxy
-    )
-  }
   url <- "https://opendata.rte-france.com/api/records/1.0/search/"
   cli <- crul::HttpClient$new(url = url)
+  if (!is.null(user) & !is.null(proxy_pwd)) {
+    cli$proxies <- crul::proxy(
+      url = curl::ie_get_proxy_for_url("https://httpbin.org/get"), 
+      user = user, pwd = proxy_pwd
+    )
+  }
   if (!is.null(from)) {
     from <- sprintf("date_heure>=%s", format(from, format = "%Y-%m-%d"))
   }
