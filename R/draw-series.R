996b551babe0691373e0ff66fcceb3a57eb154ab
pvictor <perrier.victor@gmail.com>
2018-03-26 12:04:20 +0200

    added draw series

---
+++ R/draw-series.R
@@ -0,0 +1,36 @@
+
+#' Draw a time series
+#'
+#' @param data Data to visualize, an \code{antaresDataTable} object.
+#' @param serie Name of the serie.
+#' @param mcYears Monte-Carlo years to represent.!
+#' @param main Title for the chart.
+#'
+#' @return a \code{dygraphs} object.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # TODO
+#'
+#' }
+#' @importFrom data.table dcast as.xts.data.table
+#' @importFrom dygraphs dygraph dyRangeSelector dyLegend dyHighlight dyCSS dyOptions dyAxis
+draw_series <- function(data, serie = "LOAD", mcYears = 1:51, main = serie) {
+
+  stopifnot(inherits(x = data, what = "antaresDataTable"))
+
+  dat <- data[mcYear %in% mcYears]
+  dat <- dcast(data = dat, formula = time ~ mcYear, value.var = serie)
+
+  dygraph(data = as.xts.data.table(dat), main = main) %>%
+    dyRangeSelector() %>%
+    dyLegend(show = "always") %>%
+    dyHighlight(highlightCircleSize = 3)%>%
+    dyLegend(show = "always")%>%
+    dyCSS(css = system.file('www/css_dygraph.css', package = 'antaresWeeklyMargin'))%>%
+    dyOptions(colors = rep("gray", 51)) %>%
+    dyOptions(useDataTimezone = TRUE)  %>%
+    dyAxis(name = "y", label = "MW")
+}
