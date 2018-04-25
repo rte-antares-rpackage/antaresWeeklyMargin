96d617f7ed47b8b172d0920e46be582bebccc7f5
pvictor <perrier.victor@gmail.com>
2018-04-25 15:55:17 +0200

    fix check

--- R/zzz.R
+++ R/zzz.R
@@ -13,4 +13,5 @@ utils::globalVariables(c(".SD", ".I", ".N", "groupe", "code_groupe",
                          "CHP", "Other", "Waste", "cogeneration_continue", "cogeneration_mdse_dispatchable_",
                          "file_name", "tz", "timeId",
                          "Total_fil_eau", "file_name", "hydraulique_fil_eau_eclusee", "hydraulique_tiers",
-                         "groupe_hydro", "selection", "time"))
+                         "groupe_hydro", "selection", "time",
+                         "expectation", "ror_share"))
