868bcf9ec20a8f6b124069f03384ec11a378de9c
pvictor <perrier.victor@gmail.com>
2018-06-29 11:27:51 +0200

    arguments in sim_wm

--- R/create_wm_load_fr.R
+++ R/create_wm_load_fr.R
@@ -3,6 +3,7 @@
 #'
 #' @param path Path to CNES directory, see \code{\link{read_cnes}} for usage.
 #' @param start Beginning of the simulation.
+#' @param start_prev_hebdo Beginning of previsions.
 #' @param type Forecast to use \code{prevu} or \code{premis}.
 #' @param opts
 #'   List of simulation parameters returned by the function
@@ -16,13 +17,13 @@
 #' create_wm_load_fr("Prev_hebdo_CNES/", "2018-02-27", "prevu")
 #' 
 #' }
-create_wm_load_fr <- function(path, start, type = c("prevu", "premis"), opts = antaresRead::simOptions()) {
+create_wm_load_fr <- function(path, start, start_prev_hebdo, type = c("prevu", "premis"), opts = antaresRead::simOptions()) {
   
   inputPath <- opts$inputPath
   
   type <- match.arg(type)
   start <- as.Date(start)
-  previous_sam <- get_previous(what = "samedi", start)
+  previous_sam <- as.Date(start_prev_hebdo)
   next_sam <- previous_sam + 7
   rep_prev <- list.files(path = path, pattern = format(start, "%d%m%y"))
   if (length(rep_prev) != 1) {
--- R/sim_wm.R
+++ R/sim_wm.R
@@ -2,8 +2,10 @@
 #' Setup a WM simulation before running Antares
 #'
 #' @param date_prev Date of simulation.
-#' @param start_prev_hebdo Date of forecasts.
+#' @param start_prev_hebdo Date of forecasts, with format \code{\%Y-\%m-\%d}.
 #' @param path_inputs A \code{list} with path to inputs directories, obtained with \code{path_sim_wm}.
+#' @param type_load Forecast to use \code{prevu} or \code{premis}.
+#' @param dispo_pump Pumpage availability.
 #' @param opts
 #'   List of simulation parameters returned by the function
 #'   \code{antaresRead::setSimulationPath}
@@ -20,10 +22,13 @@
 #' # todo
 #' 
 #' }
-sim_wm <- function(date_prev, start_prev_hebdo, path_inputs = path_sim_wm(), opts = antaresRead::simOptions()) {
+sim_wm <- function(date_prev, start_prev_hebdo, path_inputs = path_sim_wm(), type_load = "prevu", 
+                   dispo_pump = c(3520,3520,3520,3520,3520,3520,3520), opts = antaresRead::simOptions()) {
   
   inputPath <- opts$inputPath
   opts <- clear_wm_study(opts)
+  
+  startday <- format(as.Date(start_prev_hebdo), format = "%A")
   
   # Add Meteologica forecast
   heure_prev_meteologica <- "00:00:00"
@@ -33,24 +38,24 @@ sim_wm <- function(date_prev, start_prev_hebdo, path_inputs = path_sim_wm(), opt
   create_wm_ts(data = formet, start = start_prev_hebdo, opts = opts)
   
   # Add CNES forecast
-  opts <- create_wm_load_fr(path = path_inputs$cnes, date_prev, "prevu")
+  opts <- create_wm_load_fr(path = path_inputs$cnes, start = date_prev, start_prev_hebdo = start_prev_hebdo, type = type_load, opts = opts)
   
   sup <- read_planning(path = path_inputs$planning)
   opts <- create_wm_cluster(data = sup, start = start_prev_hebdo, opts = opts)
   
   oa <- read_forfait_oa(path = path_inputs$forfait_oa)
-  opts <- create_wm_ror(data = oa, start= date_prev,  startday = "samedi", opts = opts)
+  opts <- create_wm_ror(data = oa, start= date_prev,  startday = startday, opts = opts)
   opts <- create_wm_misc(data = oa, start = start_prev_hebdo, opts = opts)
   
   # NTC
   ntc <- fread(file = path_inputs$ntc)
-  opts <- create_wm_ntc(data = ntc, start = start_prev_hebdo, opts = opts, startday = "samedi")
+  opts <- create_wm_ntc(data = ntc, start = start_prev_hebdo, opts = opts, startday = startday)
   
   ntc_tp <- read_ntc(path = path_inputs$ntc_tp)
   opts <- create_wm_ntc_tp(data = ntc_tp, start = start_prev_hebdo, opts = opts)
   
   # HYDRAULIQUE RESERVOIR + STEP
-  dispo_pump_d <- c(3520,3520,3520,3520,3520,3520,3520)
+  # dispo_pump_d <- c(3520,3520,3520,3520,3520,3520,3520)
   
   #Add Hydro for France (forecast producteur)
   opts <- create_wm_hydro_fr(
@@ -58,7 +63,7 @@ sim_wm <- function(date_prev, start_prev_hebdo, path_inputs = path_sim_wm(), opt
     path_hydro = path_inputs$hydro, 
     start = start_prev_hebdo, 
     opts = opts, 
-    dispo_pump = dispo_pump_d
+    dispo_pump = dispo_pump
   )
   #Add Hydro for other areas
   opts <- create_wm_hydro_areas(start = start_prev_hebdo, opts = opts)
